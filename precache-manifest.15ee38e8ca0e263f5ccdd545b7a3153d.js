5f90136fb0d9034c878fbb4d29b6261657e28208
rahulmr96 <rahulmranga@gmail.com>
2022-12-20 13:30:30 -0600

    Updates

---
+++ precache-manifest.15ee38e8ca0e263f5ccdd545b7a3153d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6db4662409f5dd474c43",
+    "url": "/static/css/main.c272baf9.chunk.css"
+  },
+  {
+    "revision": "6db4662409f5dd474c43",
+    "url": "/static/js/main.6db46624.chunk.js"
+  },
+  {
+    "revision": "d5a82794cb252de57f41",
+    "url": "/static/js/1.d5a82794.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23cabdb300a839fa327cddb8d06a6aeb",
+    "url": "/index.html"
+  }
+];
